97215785a3c68f2bec6e8149830e95d606d19ec3
Mitchell <Sotto>
2022-02-09 20:16:13 -0500

    added discount example

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 var myAge = window.prompt("How old are you?")
+myAge = parseInt(myAge)
 console.log(myAge)
+
+// Give a 50% discount if older than 55, a 25% discount if 55, and no discount if younger than 55
+
+debugger
+if(myAge > 55) {
+    console.log('50%!')
+} 
+if (myAge == 55) {
+    console.log('25%')
+} else {
+    console.log('no discount')
+}
